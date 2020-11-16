e6003c036734ab9136eae5bee8f140f1e94c0a87
Falena30 <dasuyikno@gmail.com>
2020-11-16 08:08:40 +0700

    second commmit

--- second.go
+++ second.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 //fungsi
-func printText() int {
-	return 10
+func multiplay(angka1 int, angka2 int) int {
+	return angka1 * angka2
 }
 
+func getBio(umur int, nama string, status string) (string, string) {
+	umurTampung := strconv.Itoa(umur)
+	return nama + " adalah seorang " + status,
+		" yang berumur " + umurTampung + " tahun ini"
+}
 func main() {
-	fmt.Println("Nilai dari function adalah", printText())
+	fmt.Println("Nilai dari function adalah", multiplay(3, 33))
+	basicInfo, AgeInfo := getBio(22, "Dimas", "jobless")
+	fmt.Println(basicInfo, AgeInfo)
 }
