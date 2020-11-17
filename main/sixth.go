672e7aae1deaa65ab35e6ca2ff408d5696eb371f
Falena30 <dasuyikno@gmail.com>
2020-11-17 11:22:28 +0700

    now i know how to using method in another file

--- main/sixth.go
+++ main/sixth.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Go-learn/models"
 )
 
 func main() {
+
 	user1 := models.Member{
 		Nama: "dimas",
 		Umur: 24,
 	}
+	//jadi jika variabel sudah di assignment maka variabel itu sudah memiliki perintahnya sendiri
+	//jadi tidak perlu lagi memanggil packagenya
+	user1.GetInfo()
 
-	fmt.Println(user1)
+	//fmt.Println(user1)
 }
--- models/member.go
+++ models/member.go
@@ -1,7 +1,14 @@
 package models
 
+import "fmt"
+
 //Member package berfungsi sebagai pointer
 type Member struct {
 	Nama string
 	Umur int
+}
+
+//GetInfo method digunakan oleh user untuk mengetahui namanya
+func (M Member) GetInfo() {
+	fmt.Println("halo nama saya adalah")
 }
