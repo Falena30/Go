fc9043ed4fe8aaab9544fb90175d1a24403d0bb7
Falena30 <dasuyikno@gmail.com>
2020-11-17 10:23:04 +0700

    change folder location

--- main/sixth.go
+++ main/sixth.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"Go/models"
+)
 
+func main() {
+	user1 := models.member("dimas",24)
+	user1.umur = 23
+	user2 := user1
+	user2.nama = "Wahib"
+	fmt.Println(user2)
 }
---
+++ models/member.go
@@ -0,0 +1,7 @@
+package models
+
+//catatan jangan sampai nama folder sama dengan nama file, bisa jadi errorc
+type member struct {
+	nama string
+	umur int
+}
